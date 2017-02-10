f01ba2bc700ecfc3391f2fca613db39e8b2b96ba
JSteeleIR <JSteele897@gmail.com>
2017-02-10 02:24:09 -0800

    Starting to add Celery worker implementation for upload features.

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,9 +7,12 @@ sed -i -e "s#SECRET_KEY = u''#SECRET_KEY = u'$SECRET'#" /etc/timesketch.conf
 sed -i -e "s#ELASTIC_HOST = u'.*'#ELASTIC_HOST = u'elasticsearch'#" /etc/timesketch.conf
 sed -i -e "s#ELASTIC_PORT = .*#ELASTIC_PORT = $EL_PORT#" /etc/timesketch.conf
 sed -i -e "s#SQLALCHEMY_DATABASE_URI = '.*'#SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:$POSTGRES_PASSWORD@postgres/timesketch'#" /etc/timesketch.conf
+sed -i -e "s#UPLOAD_ENABLED = .*#UPLOAD_ENABLED = True#" /etc/timesketch.conf
+sed -i -e "s#CELERY_BROKER_URL='.*'#CELERY_BROKER_URL = 'redis://redis:$REDIS_PORT'#" /etc/timesketch.conf
+sed -i -e "s#CELERY_RESULT_BACKEND='.*'#CELERY_RESULT_BACKEND = 'redis://redis:$REDIS_PORT'#" /etc/timesketch.conf
 
 #/usr/bin/env
-#/bin/cat /etc/timesketch.conf
+/bin/cat /etc/timesketch.conf
 tsctl add_user -u timesketch -p timesketch
 tsctl runserver -h 0.0.0.0 -p 5000 
 #/bin/bash
